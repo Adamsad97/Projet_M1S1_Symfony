ea096917dd7e38d80090e57a2f40112d86b436d9
AdamaDiawara <diawaraad97@gmail.com>
2025-06-13 15:19:59 +0200

    Form inscription

---
+++ src/Form/RegisterUserType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegisterUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', EmailType::class, [
+                'label' => 'Votre adresse email',
+                'attr' => [
+                    'placeholder' => 'Obligatoire*'
+                ]
+            ])
+            ->add('password', passwordType::class, [
+                'label' => 'Votre mot de passe',
+                'attr' => [
+                    'placeholder' => 'Obligatoire*'
+                ]
+            ] )
+            ->add('firstname', TextType::class, [
+            'label'=>'Votre prénom',
+                'attr' => [
+                    'placeholder' => 'Obligatoire*'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+            'label'=>'Votre nom de famille',
+                'attr' => [
+                    'placeholder' => 'Obligatoire*'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Valider',
+                'attr' => [
+                    'class' => 'btn btn-success'
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
