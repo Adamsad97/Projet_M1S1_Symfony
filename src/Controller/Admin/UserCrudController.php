8835d36a09ccc7cfd34ed95d4de5a920b62e288d
AdamaDiawara <diawaraad97@gmail.com>
2025-06-24 16:24:34 +0200

    Sécurité ADMIN

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
@@ -31,6 +32,11 @@ class UserCrudController extends AbstractCrudController
 
             TextField::new('firstname', 'Prénom'),
             TextField::new('lastname', 'Nom de famille'),
+            ChoiceField::new('roles', 'Permissions')->setHelp('Vous pouvez choisir le rôle de cet utilisateur.')->setChoices([
+                'ROLE_USER' => 'ROLE_USER',
+                'ROLE_ADMIN' => 'ROLE_ADMIN',
+                //'ROLE_BANNED' => 'ROLE_BANNED',
+            ])->allowMultipleChoices(),
             TextField::new('email', 'Email')->OnlyOnIndex(),
 
         ];
